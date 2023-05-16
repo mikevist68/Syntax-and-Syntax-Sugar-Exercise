6ededb2a6ee1888f39cc4924c3d3870ad90f2731
Michael Vistocci <mikevist68@yahoo.com>
2023-05-16 15:50:24 -0400

    completed

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System.Runtime.CompilerServices;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
+
+
+
+
+
+
+
         }
     }
 }
+
+
+
+
+
+
+
+//Inferred Typing   var firstName = "John";
+//String Interpolation
+//Ternary Operator
+
+/*
+int answer = 4;
+string response;
+if (answer < 9) 
+{
+    response = answer + " is less than nine";
+}
+else
+{
+    response = answer + " is greater than or equal to nine";
+}
+*/
